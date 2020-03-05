c94fb6084c0192269a5298b60b2079b5f9048242
sreekanth9393 <sreekanthsomavarapu@gmail.com>
2020-03-05 12:03:35 +0530

    sreekanth1

---
+++ main_app_d/data.py
@@ -0,0 +1,61 @@
+GEN = (
+    ('', 'Select...'),
+    ('M', 'Male'),
+    ('FM', 'Female'),
+    ('OT', 'Others')
+)
+
+PASSPORT = (
+    ('', 'Select...'),
+    ('Y', 'Yes'),
+    ('N','No')
+)
+
+MARITAL_STATUS = (
+    ('', 'Select...'),
+    ('M', 'Married'),
+    ('UM', 'Unmarried')
+)
+
+DESIRE_JOBTYPE = (
+    ('', 'Select...'),
+    ('WH', 'Work from Home'),
+    ('In', 'Internship'),
+    ('PT', 'PartTime'),
+    ('FT', 'FullTime'),
+    ('Govt', 'Governmentjob')
+)
+
+DESIRED_SHIFT = (
+    ('', 'Select...'),
+    ('Day', 'Day Shift'),
+    ('Night', 'Day Shift'),
+    ('Rotational', 'Rotational Shift'),
+)
+
+QUALIFICATION = (
+    ('', 'Select...'),
+    ('SSC', 'SSC'),
+    ('Intermediate', 'Intermediate'),
+    ('Graduation', 'Graduation'),
+    ('Post Graduation', 'Post Graduation'),
+    ('Masters Degree', 'Masters Degree'),
+)
+
+Hiring_Process = (
+    ('', 'Select...'),
+    ('Face to face', 'Face to face'),
+    ('Written test', 'Written test'),
+    ('Teliphonic', 'Teliphonic'),
+    ('Group discussion', 'Group discussion'),
+    ('Walk in', 'Walk in')
+)
+
+Eligible_for = (
+    ('', 'Select...'),
+    ('Male','Male candidates only'),
+    ('Female','Female candidates only'),
+    ('Both','Male and Female candidates')
+)
+
+
--- main_app_d/form.py
+++ main_app_d/form.py
@@ -1,88 +1,30 @@
 from django import forms
-from .models import Jobseeker,Createjob,Bookmarks,Intrestedjobs
+from .models import Jobseeker, Createjob, Bookmarks, Intrestedjobs
+from .data import PASSPORT, GEN, MARITAL_STATUS, DESIRED_SHIFT, DESIRE_JOBTYPE, QUALIFICATION, Hiring_Process, Eligible_for
 
 
-GEN = (
-        ('', 'Select...'),
-        ('M', 'Male'),
-        ('FM', 'Female'),
-        ('OT', 'Others')
-    )
-CITY = (
-        ('', 'Choose...'),
-        ('', 'Choose...'),
-        ('hyd', 'hyderabad'),
-        ('ban', 'banglore'),
-        ('ch', 'chennai')
-    )
-PASSPORT=(
-        ('', 'Select...'),
-        ('Y', 'Yes'),
-        ('N','No')
-    )
-MARITAL_STATUS=(
-    ('', 'Select...'),
-    ('M', 'Married'),
-    ('UM', 'Unmarried')
-    )
-DESIRE_JOBTYPE=(
-    ('', 'Select...'),
-    ('WH', 'Work from Home'),
-    ('In', 'Internship'),
-    ('PT', 'PartTime'),
-    ('FT', 'FullTime'),
-    ('Govt' 'Governmentjob')
-)
-DESIRED_DESIGNATION=(
-    ('', 'Select...'),
-    ('PythonDev', 'PythonDeveloper'),
-    ('PHPDev', 'PhpDeveloper')
+class Jobseekers(forms.ModelForm):
+    passport = forms.ChoiceField(choices=PASSPORT)
+    gender = forms.ChoiceField(choices=GEN)
+    maritual_status = forms.ChoiceField(choices=MARITAL_STATUS)
+    job_type = forms.ChoiceField(choices=DESIRE_JOBTYPE)
+    desired_shift = forms.ChoiceField(choices=DESIRED_SHIFT)
+    qualification = forms.ChoiceField(choices=QUALIFICATION)
+
+    class Meta:
+        model = Jobseeker
+        fields = '__all__'
+        exclude = ('user',)
 
 
+class Create_job(forms.ModelForm):
+    hiring_process = forms.ChoiceField(choices=Hiring_Process)
+    job_type = forms.ChoiceField(choices=DESIRE_JOBTYPE)
+    education = forms.ChoiceField(choices=QUALIFICATION)
+    flexible_for = forms.ChoiceField(choices=DESIRED_SHIFT)
+    eligible_for = forms.ChoiceField(choices=Eligible_for)
 
-)
-TOTALWORK_EXPERIANCE=(
-
-
-
-
-
-)
-EXPECT_PACKAGE=(
-
-
-
-
-)
-DESIRED_SHIFT=(
-
-
-
-
-)
-DESIRED_INDUSTRY=(
-
-
-
-)
-AVAIL_JOIN=(
-
-
-
-
-)
-QUALIFICATION=(
-
-
-
-
-
-)
-EXPERIANCE=(
-
-
-
-
-
-
-)
+    class Meta:
+        model = Createjob
+        fields = '__all__'
+        exclude = ('user',)
